8ebd2f93e0c40c2f08b5dffc714ae38df820dd6b
Sean Foley <sean@seanfoley.com>
2021-03-28 17:57:36 -0700

    command line processing

--- main.py
+++ main.py
@@ -1,78 +1,121 @@
 import socket
 import struct
 import time
+import json
+import sys
+import getopt
 
-IPADDR = "ntp.padnet.home"
-PORTNUM = 123
-VERSION = 4
-MODE = 7
-
-# Used as a data buffer
-# request is 8 bytes, response is 48 bytes
-PACKETDATA = bytearray(8)
-
-PACKETDATA[0] = VERSION << 3 | MODE
-PACKETDATA[1] = 0      # sequence number
-PACKETDATA[2] = 0x10   # implementation code - custom
-PACKETDATA[3] = 1      # request code, use 1 for now
-
-PACKETDATA[4] = 0
-PACKETDATA[5] = 0
-PACKETDATA[6] = 0
-PACKETDATA[7] = 0
+NTP_DEFAULT_PORT_NUM = 123
 
 # reference time in seconds since 1900-01-01 00:00:00
 # for conversion from NTP time to system time
 TIME1970 = 2208988800
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-    server_address = (IPADDR, PORTNUM)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(2.5)
-
-    sent = sock.sendto(PACKETDATA, server_address)
-
-    response, server = sock.recvfrom(1024)
+# the default timeout for socket operations
+SOCKET_TIMEOUT_SECS = 10.0
 
 
-    ref_ts0 = (struct.unpack('<I', response[16:20])[0]) / 4294967296.0
-    ref_ts1 = struct.unpack('<I', response[20:24])[0]
-    uptime  = struct.unpack('<I', response[24:28])[0]
-    ntp_requests = struct.unpack('<I', response[28:32])[0]
-    cmd_served  = struct.unpack('<I', response[32:36])[0]
-    lock_time   = struct.unpack('<I', response[36:40])[0]
-  #  flags = struct.unpack('b', bytes(response[40]))[0]
-    satellites = struct.unpack_from('<B',response, 41) [0]
-    serial_number = struct.unpack('<H', response[42:44])[0]
-    firmware = struct.unpack('<I', response[44:48])[0]
+def get_ntp_metrics(host, port, timeout_sec=SOCKET_TIMEOUT_SECS):
 
-    sock.close()
+    version = 4
+    mode = 7
+
+    # Used as a data buffer
+    # request is 8 bytes, response is 48 bytes
+    packet = bytearray(8)
+
+    packet[0] = version << 3 | mode
+    packet[1] = 0     # sequence number
+    packet[2] = 0x10  # implementation code - custom
+    packet[3] = 1     # request code, use 1 for now
+
+    packet[4] = 0
+    packet[5] = 0
+    packet[6] = 0
+    packet[7] = 0
+
+    recv_buffer = 1024
+    server_address = (host, port)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.settimeout(timeout_sec)
+        sock.sendto(packet, server_address)
+        response, server = sock.recvfrom(recv_buffer)
+
+        # Not really needed from within a with context, but
+        # I like to be explicit
+        sock.close()
+
+    ref_ts0 = (struct.unpack_from('<I', response[16:20])[0]) / 4294967296.0
+    ref_ts1 = struct.unpack_from('<I', response[20:24])[0]
+    uptime = struct.unpack_from('<I', response[24:28])[0]
+    ntp_requests = struct.unpack_from('<I', response[28:32])[0]
+    # cmd_served = struct.unpack_from('<I', response[32:36])[0]
+    lock_time = struct.unpack_from('<I', response[36:40])[0]
+    flags = struct.unpack_from('<B', bytes(response[40]))[0]
+    satellites = struct.unpack_from('<B', response, 41)[0]
+    serial_number = struct.unpack_from('<H', response[42:44])[0]
+    firmware = struct.unpack_from('<I', response[44:48])[0]
 
     t = time.gmtime(ref_ts1 - TIME1970)
 
-    #print( "UTC time: %d-%02d-%02d %02d:%02d:%02.04" % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec + ref_ts0))
-
-    print( "UTC time {0}-{1}-{2} {3}:{4}:{5}".format(
+    utc = '{0}-{1:02}-{2:02}T{3:02}:{4:02}:{5:02.04}Z'.format(
         t.tm_year,
         t.tm_mon,
         t.tm_mday,
         t.tm_hour,
         t.tm_min,
-        t.tm_sec + ref_ts0))
+        t.tm_sec + ref_ts0)
 
-    print( "NTP time: {0}".format(ref_ts1 + ref_ts0))
+    ntp_time = ref_ts1 + ref_ts0
 
-    print( "Satellites locked: {0}".format(satellites))
-    print( "Lock time: {0} seconds, {1} hours, {2} days".format( lock_time, lock_time /3600, lock_time / 86400 ))
+    metrics = {
+        'serial-number': serial_number,
+        'firmware': firmware,
+        'flags': flags,
+        'uptime-hours': uptime / 3600,
+        'ntp-requests': ntp_requests,
+        'utc-time': utc,
+        'ntp-time': ntp_time,
+        'lock-time-hours': lock_time / 3600,
+        'satellites': satellites
+        }
 
-    print( "NTP requests served: {0}".format(ntp_requests))
+    return metrics
 
-# Press the green button in the gutter to run the script.
+
+def get_command_line(argv):
+
+    host = ''
+    port = NTP_DEFAULT_PORT_NUM
+
+    try:
+        opts, args = getopt.getopt(argv, "", ["ntpserver=", "port="])
+
+        for opt, arg in opts:
+            if opt == '-h':
+                 print('leo-ntp-monitor.py --ntpserver=SERVER|IP_ADDRESS --port=NTP_PORT')
+                 sys.exit()
+            elif opt in ("-ntpserver", "--ntpserver"):
+                 host = arg
+            elif opt in ("-p", "--port"):
+                 port = int(arg)
+
+        return host, port
+
+    except getopt.GetoptError:
+        print('usage:  --ntpserver=SERVERNAME --port=NTP-PORT')
+        sys.exit(2)
+
+
+def process():
+
+    host, port = get_command_line(sys.argv[1:])
+
+    metrics = get_ntp_metrics(host, port)
+
+    print(json.dumps(metrics))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    process()
